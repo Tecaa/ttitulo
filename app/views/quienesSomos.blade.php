b9baf46f672461e20794479cfad4fb7c921e62c4
Marco Rojas <fergushog@hotmail.com>
2015-08-16 13:36:16 +0000

    logo nuevo

--- app/views/quienesSomos.blade.php
+++ app/views/quienesSomos.blade.php
@@ -5,7 +5,7 @@
 			<div class="row">
 				<div class="col-md-12">
 					<ol class="breadcrumb">
-				      <li><a href="#">Home</a></li>
+				      <li><a href="#">Inicio</a></li>
 				      <li class="active">Quienes somos</li>
 				    </ol>
 				</div>
@@ -14,8 +14,8 @@
 			<div class="row">
 				<div class="col-md-8 col-sm-8">
 					<h3>Nuestra empresa</h3>
-          <p><em>Centro Naturista Masiel es una empresa de La Calera que busca dar satisfacción a sus clientes de la mano de la naturaleza.</em></p>
-					<p>Centro Naturista Masiel nace con la necesidad de brindarles a las personas productos que les otorguen bienestar de una forma más natural. Iniciamos nuestras actividades en el año 2010 estableciéndonos al interior de Home Center Sodimac de La Calera en la V región donde se encuentra hasta el día de hoy. Contamos con una amplia gama de productos a su servicio y queremos brindarle una mejor salud de mano de la naturaleza gracias a nuestros productos y una acogedora atención.</p>
+          <p><em>Joyas Sagitario es una tienda online de venta de joyas ubicada en La Calera.</em></p>
+					<p>Joyas Sagitario nace con la necesidad de brindarles a las personas joyas y bisutería con la comodidad de la compra por internet. Nos centramos principalmente en La Calera y sus alrededores pero también realizamos envíos en Chile. Revise nuestros catálogos de productos en la parte superior de nuestra página, nuestros productos irán variando e incrementándose con el tiempo.</p>
 				</div>
 				<div class="col-md-4 col-sm-4">
 					<img src="img/img01.jpg" class="img-responsive img-rounded" alt="dodolan manuk catalog template">
@@ -29,7 +29,7 @@
 				</div>
 				<div class="col-md-8 col-sm-8">
 					<h3>Nuestra misión</h3>
-					<p>Queremos acercar a nuestros clientes los mejores productos de parte de la naturaleza que logren ayudarlo a mejorar sus dolencias y también a prevenirlas. Queremos que nuestros productos no contengan aditivos químicos artificiales que pueden producir efectos adversos o negativos en la salud de las personas. Así es como llegamos a estar mucho mas cerca de nuestra clientela y con un precio más accesible trayendo cada vez más artículos buscados por ella logrando que puedan generar una confianza con nosotros y fidelidad.</p>
+					<p>Queremos brindarles a nuestros clientes una agradable y cómoda experiencia de compra de manera remota. Queremos que nuestros clientes nos reconozcan por ser una empresa confiable y de calidad.
 				</div>
 			</div>
 			
@@ -37,7 +37,7 @@
 			<div class="row padd20-top-btm">
 				<div class="col-md-12 text-center">
 					<h3>Nosotros</h3>
-					<p>¡Búsquenos en nuestra tienda dentro del Home Center La Calera! (V región). Cada vez ampliamos más nuestra variedad de productos con lo que nuestros clientes necesitan. Revise nuestra sección de productos organizados por categoría.</p>
+					<p>¡Explora nuestra página y regístrate! Cada vez ampliamos más nuestra variedad de productos con lo que nuestros clientes necesitan. Revise nuestra sección de productos organizados por categoría.</p>
 				</div>
 			</div>
 		</div>
