64973ecc5360014dcc92f8613729c0c9f21a62df
Marco Rojas <fergushog@hotmail.com>
2015-08-18 00:37:40 +0000

    .

--- app/views/layouts/page-content.blade.php
+++ app/views/layouts/page-content.blade.php
@@ -38,7 +38,7 @@
 				<div class="col-md-12">
 					<ol class="breadcrumb">
             <li><a href="/">Inicio</a></li>
-				      <li><a href={{ URL::previous() }}>Lista de productos</a></li>
+				      <!--<li><a href={{ URL::previous() }}>Lista de productos</a></li>-->
 				      <!--<li ><a href="#">Productos</a></li>-->
             <li class="active">{{$titulo}}</li>
 				    </ol>
