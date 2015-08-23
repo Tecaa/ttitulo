63ff9a0a0b4be31ec7541d54c4f739d8c39c5c4e
Marco Rojas <fergushog@hotmail.com>
2015-08-23 02:00:54 +0000

    factura codigob arras

--- public/js/detalleFactura.js
+++ public/js/detalleFactura.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     },
     data: detalles,
     columns: [
-      { "data": "codigo_producto" },
+      { "data": "codigo_barras" },
       { "data": "producto.nombre_producto" },
       { "data": "producto.proveedor.nom_proveedor" },
       { "data": "producto.cantidad" },
